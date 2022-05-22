44c7d4248e21f72056d78f4ce87167d33a7526a4
mozore <474243507@qq.com>
2022-05-22 23:36:07 +0800

    修改了FindUser()减少密码的二次查询

--- service/user/login.go
+++ service/user/login.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"golang.org/x/crypto/bcrypt"
 	userdao "simple-douyin/dao/user"
+	"simple-douyin/model"
 )
 
 // LoginFlow 控制登录流程
@@ -30,29 +31,27 @@ func NewLoginFlow(logUsername string, logPassword string) *LoginFlow {
 }
 
 func (f *LoginFlow) DoLogin() (uint32, error) {
-	userId, err := f.checkUserName()
+	ua, err := f.checkUserName()
 	// 如果用户不存在返回error
 	if err != nil {
 		return 0, errors.New(fmt.Sprintf("user:%s doesn't exit", f.logUsername))
 	}
+
 	// 如果身份验证失败返回error
-	if err := f.authentication(); err != nil {
+	if err := f.authentication(ua.Password); err != nil {
 		return 0, err
 	}
 	// 将userId和err返回给controller
-	return userId, nil
+	f.userId = ua.Id
+	return f.userId, nil
 }
 
-func (f *LoginFlow) checkUserName() (uint32, error) {
-	userId, err := f.authDao.FindUser(f.logUsername)
-	return userId, err
+func (f *LoginFlow) checkUserName() (*model.UserAuth, error) {
+	ua, err := f.authDao.FindUser(f.logUsername)
+	return ua, err
 }
 
-func (f *LoginFlow) authentication() error {
-	password, err := f.authDao.GetPassword(f.logUsername)
-	if err != nil {
-		return err
-	}
-	err = bcrypt.CompareHashAndPassword([]byte(password), []byte(f.logPassword))
+func (f *LoginFlow) authentication(password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(password), []byte(f.logPassword))
 	return err
 }
